f4273fd459c11c0def7e1ad758b42e97a4b37343
Montaniz.Stills <montanizstills@gmail.com>
2024-01-20 07:44:53 -0500

    add hello world test endpoint (for use pinging endpoint to test isalive?);
    add newUserAccepting endpoint and logging;

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/zero2dev/jira/webhooks/newUserAccepted', methods=["GET"])
+@app.route('/hello-world', methods=["GET"])
 def hello_world():
     return 'Hello World!'
 
+@app.route('/zero2dev/jira/webhooks/newUserAccepted', methods=["GET"])
+def hello_world():
+    return '\n Zero2Dev has just confirmed a new user. \n'
+
 
 if __name__ == '__main__':
+    # self-hosted TSL security
     # app.run(ssl_context=('./zero2dev-cert.pem', './zero2dev.pem'))
+
+    # incoming connection mask # 0.0.0.0 means anywhere
     app.run(host="0.0.0.0")
